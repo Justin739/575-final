63f3039334b49bfc94bc47fe07b0a0dca1f12065
Justin Wheeler <Justin_T_Wheeler@live.com>
2018-04-10 20:50:56 -0500

    Added histogram for each character in the best plate result.  Merged results to get the most likely plate result

--- main.cpp
+++ main.cpp
@@ -2,7 +2,9 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/opencv.hpp>
+#include <algorithm>
 #include <iostream>
+#include <fstream>
 #include <alpr.h>
 
 using namespace cv;
@@ -20,7 +22,6 @@ int main(int argc, char* argv[]) {
         std::cout << "Input video not found." << std::endl;
         return -1;
     }
-//
 
     // Initialize the library using United States style license plates.
     // You can use other countries/regions as well (for example: "eu", "au", or "kr")
@@ -43,19 +44,23 @@ int main(int argc, char* argv[]) {
     // Loop to read from input one frame at a time, write text on frame, and
     // copy to output video
     Mat frame;
-    namedWindow("test", 1);
+    //namedWindow("test", 1);
 
     std::vector<AlprRegionOfInterest> roi;
     input_cap.read(frame);
 
-    int width = 250;
-    int height =600;
-    Rect rect(frame.cols/2-width/2, frame.rows-height, width, height);
+    std::ofstream csvLog;
+    csvLog.open("output.csv");
+
+    int width = 500;
+    int height = 400;
+    Rect rect(frame.cols/2-width/2, frame.rows-height - 200, width, height);
     roi.push_back(AlprRegionOfInterest(rect.x, rect.y, rect.width, rect.height));
     //Size scaledSize(640, 360);
     int frameCounter = 0;
     int foundPlateCounter = 0;
     int noPlateCounter = 0;
+    std::vector<AlprPlateResult> plateReadings;
 
     while(input_cap.read(frame)) {
         frameCounter++;
@@ -87,6 +92,7 @@ int main(int argc, char* argv[]) {
             float confidenceLevel = currPlate.bestPlate.overall_confidence;
             std::vector<AlprPlate> topPlates = currPlate.topNPlates;
             std::cout << i << " " << licensePlateText << " "<< confidenceLevel << std::endl;
+            plateReadings.push_back(currPlate);
         }
 
         if (frameResults.plates.size() > 0) {
@@ -96,19 +102,67 @@ int main(int argc, char* argv[]) {
             noPlateCounter++;
 
             if (noPlateCounter >= 5) {
-                std::cout << "MIDDLE OF CARS" << std::endl;
+                //std::cout << "MIDDLE OF CARS" << std::endl;
+                // <Process the last car here>
+                std::vector<char> indivChars[plateReadings.size()];
+                int licensePlateLen = 0;
+                std::map<int, int> maxLicenseLen;
+
+                for (int i = 0; i != plateReadings.size(); i++) {
+                    std::string plateChars = plateReadings[i].bestPlate.characters;
+
+                    if (maxLicenseLen.find((int) plateChars.length()) != maxLicenseLen.end()) {
+                        maxLicenseLen[(int) plateChars.length()]++;
+                    } else {
+                        maxLicenseLen.insert(std::make_pair((int) plateChars.length(), 1));
+                    }
+
+                    for (int j = 0; j < plateChars.length(); j++) {
+                        //std::cout << plateChars[j] << std::endl;
+                        indivChars[i].push_back(plateChars[j]);
+                    }
+                }
+
+                std::map<int, int>::iterator iter;
+                int maxFreq = 0;
+
+                for (iter = maxLicenseLen.begin(); iter != maxLicenseLen.end(); iter++) {
+                    if (iter->second > maxFreq) {
+                        maxFreq = iter->second;
+                        licensePlateLen = iter->first;
+                    }
+                }
+
+                char histogram[128] = {};
+                char plateString[licensePlateLen] = {};
+
+                for (int i = 0; i < licensePlateLen; i++) {
+                    for (int j = 0; j < plateReadings.size(); j++) {
+                        if (i < indivChars[j].size()) {
+                            histogram[indivChars[j][i]]++;
+                        }
+                    }
+
+                    char mostLikelyChar = (char) std::distance(histogram, std::max_element(histogram, histogram + sizeof(histogram) / sizeof(char)));
+                    plateString[i] = mostLikelyChar;
+                    std::fill_n(histogram, sizeof(histogram) / sizeof(char), 0);
+                }
+
+                for (int i = 0; i < licensePlateLen; i++) {
+                    std::cout << plateString[i];
+                    csvLog << plateString[i];
+                }
+
+                std::cout << "" << std::endl;
+                csvLog << "\n";
+
                 foundPlateCounter = 0;
                 noPlateCounter = 0;
+                plateReadings.clear();
             }
         }
 
         rectangle(frame, rect, Scalar(255,255,0));
-        putText(frame, "Hello World!",
-                Point(0, 50),
-                FONT_HERSHEY_PLAIN,
-                1.0,
-                Scalar(255, 255, 255));
-
         imshow("test", frame);
 
         char keyPressed = waitKey(30);
@@ -123,5 +177,6 @@ int main(int argc, char* argv[]) {
 
     // free the capture objects from memory
     input_cap.release();
+    csvLog.close();
     return 0;
 }
